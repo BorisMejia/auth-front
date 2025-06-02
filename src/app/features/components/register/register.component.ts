fef04e34899541e996b9a363b83f0667dc159773
BorisMejia <borisfernandez916@gmail.com>
2025-06-01 19:31:47 -0500

    feat: implement registration form with two-way data binding and submit functionality

--- src/app/features/components/register/register.component.ts
+++ src/app/features/components/register/register.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/service/login/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export default class RegisterComponent {
 
+  name: string = '';
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  register(){
+    this.authService.register(this.name, this.email, this.password).subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: (err) => console.log('Register failed', err) 
+    });
+  }
 }
